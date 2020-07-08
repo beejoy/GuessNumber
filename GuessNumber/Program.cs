e0b597d6d76a43c067760d5a53f4fb52e37a5afe
Bijay Rai <b33j0y@live.com>
2020-07-08 21:00:53 +0545

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToGuess = new Random().Next(101);
+
+            Console.WriteLine("Welcome Contestant!");
+            do
+            {
+                int userGuess = -1;
+                Console.WriteLine("Take a guess what I'm thinking... Any number between 0 and 100.");
+                if (int.TryParse(Console.ReadLine(), out userGuess))
+                {
+                    if (userGuess == numberToGuess)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Not quite. I'm thinking of a number that is {(userGuess > numberToGuess ? "lower" : "higher")}.");
+                }
+                else
+                {
+                    userGuess = -1;
+                    Console.WriteLine("Please enter a numberic value.");
+                }
+            } while (true);
+
+            Console.WriteLine($"Well done!!! You guessed right. I was thinking of {numberToGuess}.");
+            Console.WriteLine("Thanks for playing.");
+            Console.ReadLine();
+        }
+    }
+}
